4bb751ad2c70c63fa1aeecdad3413a80b6212512
MKO1640 <micha.kurzweil@gmail.com>
2020-05-30 23:40:57 +0200

    Update hive_record.h

--- lib/hiveeyes/hive_record.h
+++ lib/hiveeyes/hive_record.h
@@ -9,7 +9,12 @@ typedef struct {
   float humidity_outside;
   float temperature_inside_1;
   float temperature_inside_2;
+  float temperature_inside_3;
+  float temperature_inside_4;
+  float temperature_inside_5;
   float weight;
+  float voltage;                
+  float rssi; 
 } Hive_record_type;
 
 #endif /* ifndef HIVE_RECORD_H_ */
